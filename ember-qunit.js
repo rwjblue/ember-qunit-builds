74c33a50a55125004d346e3eb96dec95e0e91e76
Tomster <tomster@emberjs.com>
2014-11-29 00:58:16 +0000

    ember-qunit: https://github.com/rwjblue/ember-qunit/commits/a499c2fdd03fd52fce47ddda9ac21cf83316a21f.

--- ember-qunit.js
+++ ember-qunit.js
@@ -189,11 +189,28 @@ define("ember-qunit",
     __exports__["default"] = function isolatedContainer(fullNames) {
       var resolver = getResolver();
       var container = new Ember.Container();
+      var normalize = function(fullName) {
+        return resolver.normalize(fullName);
+      };
+      //normalizeFullName only exists since Ember 1.9
+      if (Ember.typeOf(container.normalizeFullName) === 'function') {
+        container.normalizeFullName = normalize;
+      } else {
+        container.normalize = normalize;
+      }
       container.optionsForType('component', { singleton: false });
       container.optionsForType('view', { singleton: false });
       container.optionsForType('template', { instantiate: false });
       container.optionsForType('helper', { instantiate: false });
       container.register('component-lookup:main', Ember.ComponentLookup);
+      container.register('controller:basic', Ember.Controller, { instantiate: false });
+      container.register('controller:object', Ember.ObjectController, { instantiate: false });
+      container.register('controller:array', Ember.ArrayController, { instantiate: false });
+      container.register('view:default', Ember._MetamorphView);
+      container.register('view:toplevel', Ember.View.extend());
+      container.register('view:select', Ember.Select);
+      container.register('route:basic', Ember.Route, { instantiate: false });
+
       for (var i = fullNames.length; i > 0; i--) {
         var fullName = fullNames[i - 1];
         var normalizedFullName = resolver.normalize(fullName);
